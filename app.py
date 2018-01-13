90bca90220e130043c4bdf2cf15e8cc3f4533557
DNQ1408 <hoadd.2014@gmail.com>
2018-01-14 02:03:29 +0700

    ...

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
+
 from flask import Flask, render_template
-=======
 from flask import Flask, render_template,request,redirect,url_for
 from random import choice
 from models.info import Customer
 
->>>>>>> 0090f6241b59b80014c36bfb60d8596868431102
+
 app = Flask(__name__)
 
 
@@ -13,14 +12,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-<<<<<<< HEAD
+
 @app.route('/service')
 def service():
     return render_template('service.html')
 @app.route('/pay')
 def pay():
     return render_template('pay.html')
-=======
+
 @app.route('/form', methods = ["GET","POST"])
 def form():
     if request.method == "GET":
@@ -37,6 +36,6 @@ def form():
         new_order.save()
         return render_template('thankyou.html')
 
->>>>>>> 0090f6241b59b80014c36bfb60d8596868431102
+
 if __name__ == '__main__':
   app.run(debug=True)
