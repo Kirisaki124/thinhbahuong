de6fa57336345b6af777f97a16806fddf1d7e005
DNQ1408 <hoadd.2014@gmail.com>
2018-01-21 09:41:49 +0700

    ...

--- static/javascript/service.js
+++ static/javascript/service.js
@@ -1,9 +1,28 @@
-$(document).ready(function() {
-  var num_package = 0;
-  $("#add_cart_1").click(function(event){
-    num_package += 1;
-    console.log(num_package);
 
-    $()
+$(document).ready(function() {
+  var total_package = 0;
+  var package_1 = 0;
+  var package_2 = 0;
+  var package_3 = 0;
+  function cart () {
+    var i = document.getElementById('cart');
+    i.innerHTML = ' ' + total_package;
+  };
+
+  $("#add_cart_1").click(function(event){
+    total_package +=1;
+    package_1 +=1;
+    cart();
+  });
+  $("#add_cart_2").click(function(event){
+    total_package +=1;
+    package_2 +=1;
+
+    cart();
+  });
+  $("#add_cart_3").click(function(event){
+    total_package +=1;
+    package_3 +=1;
+    cart();
   });
 });
